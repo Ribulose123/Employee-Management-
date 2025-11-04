44ffa61eba5fd88859785819db899fbd0862dbe3
Ribulose <okposiokparobor@gmail.com>
2025-11-04 03:16:33 +0100

    Employee Migration added

---
+++ EmployeeManagement.Domain/Employee.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagement.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public EmployeeStatus Status { get; set; } = EmployeeStatus.Active;
+        public string? Position { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        [Required]
+        [PastOrTodayDate(ErrorMessage = "Date of hire must be today or past")]
+        public DateTime DateHired { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; }
+        public Department? AssignedDepartment { get; set; }
+    }
+}
---
+++ EmployeeManagement.Persistence/EmployeeManagementDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using EmployeeManagement.Domain;
+namespace EmployeeManagement.Persistence
+{
+    public class EmployeeManagementDbContext : DbContext
+    {
+        public EmployeeManagementDbContext(DbContextOptions<EmployeeManagementDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Salary)
+                .HasPrecision(18, 2);
+            
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Employee> Employees
+        {
+            get; set;
+        }
+    }
+}
